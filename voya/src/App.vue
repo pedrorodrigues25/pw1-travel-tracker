42f31d6ba9a77184cfa91fc2385d6c8efa5993a8
pedro_rodrigues_25 <40240239@esmad.ipp.pt>
2025-11-24 14:30:33 +0000

    mudança da img da logo

--- voya/src/App.vue
+++ voya/src/App.vue
@@ -5,7 +5,7 @@
 
       <div class="login-card">
         <img src="/src/img/Vector.png" alt="Decorative flight vector" class="vector-art" />
-        <img src="/src/img/logo.png" alt="Voya Logo" style="width: 20%" />
+        <img src="/src/img/logo.svg" alt="Voya Logo" style="width: 20%" />
         <p style="font-size: 20px; color: #014f76">Login to your account</p>
 
         <label>Email</label>

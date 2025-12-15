c6be6c3a5ecbc9d9adc5970dc7230d5a7369eda0
Mcaldas11 <miguelmeniciocaldas@gmail.com>
2025-12-15 15:45:19 +0000

    interesses

--- voya/src/router/index.js
+++ voya/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LandingView from '../views/LandingView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import InterestsView from '../views/InterestsView.vue'
 import DestinationsView from '../views/DestinationsView.vue'
 
 const router = createRouter({
@@ -21,6 +22,11 @@ const router = createRouter({
       path: '/register',
       component: RegisterView,
       name: 'Register'
+    },
+    {
+      path: '/interests',
+      component: InterestsView,
+      name: 'Interests'
     },
     {
       path: '/destinations',
---
+++ voya/src/views/InterestsView.vue
@@ -0,0 +1,7 @@
+<template>
+  <InterestsForm />
+</template>
+
+<script setup>
+import InterestsForm from '../components/InterestsForm.vue'
+</script>
